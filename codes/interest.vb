4b12384b65f2048b4b751ba815a07b34e88d77d6
0822065126toey <122332543+0822065126toey@users.noreply.github.com>
2023-01-10 10:02:48 +0700

    Add files via upload

---
+++ codes/interest.vb
@@ -0,0 +1,17 @@
+﻿Imporst system
+Module Module1
+    Public deposit, rate As Double
+    Sub Main()
+        Dim year1, year2 As Double
+        rate = 0.02
+        deposit = 10000
+
+        year1 = deposit * (1 + rate)
+        REM Console.Writeline("First year={0}",year1)
+        year2 = year1 * (1 + rate)
+        Console.WriteLine("second year={0 d}", year2)
+        Console.ReadLine()
+
+    End Sub
+
+End Module
